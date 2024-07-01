0d774d3ac313367f7eff598bdb1ee969b6dfe9d4
Igor <igorspinoladev@gmail.com>
2024-07-01 15:09:48 -0300

    typescript added

--- server/server.ts
+++ server/server.ts
@@ -1,15 +1,16 @@
+import { Request, Response } from "express"
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = 8080;
 
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({message: "hello backend"});
+app.get("/", (req: Request, res: Response) => {
+  res.send({message: "hello backend"});
 })
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 })
-
