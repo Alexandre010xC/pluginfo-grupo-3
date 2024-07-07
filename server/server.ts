bd13d0a14c05937382dd99ed81f43cf925f15c98
Igor <igorspinoladev@gmail.com>
2024-07-07 00:40:47 -0300

    delete, edit, get unique product routes created

--- server/server.ts
+++ server/server.ts
@@ -21,12 +21,33 @@ app.get("/get_products", async (req: Request, res: Response) => {
     const products = await prisma.product.findMany()
     res.send({
       message: "products retrieved succesfully",
-      products: products
+      products: products,
     });
   } catch (error: any) {
       console.error("Erro ao listar produtos", error);
       res.send({
         message: "error when getting product list",
+      });
+  }
+})
+
+app.get("/get_product/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+
+  try {
+    const product = await prisma.product.findUnique({
+      where: {
+        id:id
+      }
+    })
+    res.send({
+      message: "product retrieved succesfully",
+      product: product
+    });
+  } catch (error: any) {
+      console.error("Erro ao listar o produto", error);
+      res.send({
+        message: "error when getting product",
       });
   }
 })
@@ -47,12 +68,64 @@ app.post("/create_product", async (req: Request, res: Response) => {
     })
     res.send({
       message: "product added succesfully",
-      product: product
+      product: product,
     });
   } catch (error: any) {
       console.error("Erro ao criar produto:", error);
       res.send({
         message: "error when creating product",
+      });
+  }
+
+})
+
+app.post("/edit_product", async (req: Request, res: Response) => {
+  const { name, brand, price, desc, tags, img_src, color, id } = req.body
+  try {
+    const product = await prisma.product.update({
+      where: {
+        id:id
+      },
+      data: {
+        name: name,
+        brand: brand,
+        price: parseFloat(price),
+        description: desc,
+        tags: tags,
+        image_source: img_src,
+        color: color
+      }
+    })
+    res.send({
+      message: "product edited succesfully",
+      product: product,
+    });
+  } catch (error: any) {
+      console.error("Erro ao editar produto:", error);
+      res.send({
+        message: "error when editing product",
+      });
+  }
+
+})
+
+app.delete("/delete_product/:id", async (req: Request, res: Response) => {
+  const id = parseInt(req.params.id)
+
+  try {
+    const product = await prisma.product.delete({
+      where: {
+        id:id
+      },
+    })
+    res.send({
+      message: "product deleted succesfully",
+      product: product,
+    });
+  } catch (error: any) {
+      console.error("Erro ao deleter produto:", error);
+      res.send({
+        message: "error when deleting product",
       });
   }
 
