42500af7d8f9c41322644c0c801d12d1ae148216
Alexandre Coelho <alexandre.010c@gmail.com>
2024-07-07 23:00:09 -0300

    WIP: products components and pages

---
+++ client/src/app/components/ProductCard.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Image, { ImageProps } from "next/image";
+import styles from './ProductCard.module.css';
+
+import favoriteIcon from '@/assets/utilitary/like.svg'
+
+interface ProductCardProp {
+  image: ImageProps['src'];
+  name: string;
+  description: string;
+  price: number;
+}
+
+const ProductCard: React.FC<ProductCardProp> = ({image, name, description, price}) => {
+  const formatted_price = new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits:2
+  }).format(price);
+
+  return (
+    <div className={styles.productCard}>
+      <Image src={image} alt={name} />
+      <div>
+        <h6>{name}</h6>
+        <p>{description}</p>
+        <div className={styles.otherInfo}>
+          <span className={styles.price}>R${formatted_price}</span>
+          <Image className={styles.favoriteIcon} src={favoriteIcon} alt='Salvar como favorito' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
